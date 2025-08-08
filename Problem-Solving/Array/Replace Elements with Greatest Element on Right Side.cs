a42ddeb02c5022ee27599c929634e2d53306dc9c
mohamedabdelstar06 <mohamedabdelstar06@gmail.com>
2025-08-08 20:47:43 +0300

    Add files via upload

---
+++ Problem-Solving/Array/Replace Elements with Greatest Element on Right Side.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+	public int[] ReplaceElements(int[] arr)
+	{
+		int max = -1;
+		for (int i = arr.Length -1; i>=0; i--)
+		{
+			int current = arr[i];
+			arr[i]= max;
+
+			if (current > max)
+				max = current;
+
+
+		}
+		return arr;
+	}
+}
